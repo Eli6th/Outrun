d3ce4cbec13bde765602c5b7ecd9514cec6d8cca
Eli <eliolcott@gmail.com>
2022-12-23 16:28:57 -0500

    Edited the share page

--- src/share.js
+++ src/share.js
@@ -2,7 +2,7 @@ function shareScore() {
     if (localStorage.getItem('last_run_score')) var current_level = parseInt(localStorage.getItem('last_run_score'));
     else var current_level = document.getElementById("level_counter").innerHTML;
 
-    navigator.clipboard.writeText("Outrun \nLevel: " + current_level);
+    navigator.clipboard.writeText("Outrun \nLevel: " + current_level + "\nhttps://symphonious-taiyaki-858f51.netlify.app/");
 
     var x = document.getElementById("notification_box");
     x.className = "show";
