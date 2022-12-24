06238e8241399a3ce1d6ba3a3c82d53e35fe2195
Eli <eliolcott@gmail.com>
2022-12-24 00:05:41 -0500

    Edited share link

--- src/share.js
+++ src/share.js
@@ -2,7 +2,7 @@ function shareScore() {
     if (localStorage.getItem('last_run_score')) var current_level = parseInt(localStorage.getItem('last_run_score'));
     else var current_level = document.getElementById("level_counter").innerHTML;
 
-    navigator.clipboard.writeText("Outrun \nJust got to level: " + current_level + "\nhttps://symphonious-taiyaki-858f51.netlify.app/");
+    navigator.clipboard.writeText("Outrun \nJust got to level: " + current_level + "\nhttps://outrun.eliolcott.com");
 
     var x = document.getElementById("notification_box");
     x.className = "show";
