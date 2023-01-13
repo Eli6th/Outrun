75a9c6f2ad60874b5b7ec3f88038e08e67013e80
Eli <eliolcott@gmail.com>
2023-01-12 20:55:27 -0500

    Edited the share button to work

--- src/share.js
+++ src/share.js
@@ -1,7 +1,7 @@
 function shareScore() {
     if (localStorage.getItem('last_run_score')) var current_level = parseInt(localStorage.getItem('last_run_score'));
     else var current_level = document.getElementById("level_counter").innerHTML;
-    if (localStorage.getItem('last_run_world')) var last_run_world = localStorage.getItem('last_run_score');
+    if (localStorage.getItem('last_run_world')) var last_run_world = localStorage.getItem('last_run_world');
     else var last_run_world = document.getElementById("world_counter").innerHTML;
 
     navigator.clipboard.writeText("Outrun \nJust got to level: " + current_level + " in " + last_run_world + "\nhttps://outrun.eliolcott.com");
